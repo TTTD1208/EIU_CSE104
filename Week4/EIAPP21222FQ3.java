492a619d019d7ec94322f7d782948d2e3d9f2617
TTTD1208 <asphalt12082006@gmail.com>
2026-01-27 13:34:36 +0700

    thêm bài tập

--- Week4/EIAPP21222FQ3.java
+++ Week4/EIAPP21222FQ3.java
@@ -3,6 +3,17 @@ import java.util.Scanner;
 public class EIAPP21222FQ3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        double N = sc.nextDouble();
+        int M = sc.nextInt();
+        double money = N;
+        double[] rate = new double[] {
+                0.0390 / 12, 0.0392 / 12, 0.0395 / 12, 0.0399 / 12, 0.0404 / 12, 0.0554 / 12, 0.0572 / 12, 0.0592 / 12,
+                0.0614 / 12, 0.0638 / 12, 0.0664 / 12, 0.0692 / 12
+        };
+        while (money > 0) {
+            
+        }
     }
+
+    
 }
